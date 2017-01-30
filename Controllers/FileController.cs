5bd4526466eff62935f9991552fa68cfcbc5b987
boltean <olteanb@hotmail.com>
2017-01-29 18:53:32 -0800

    first commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,53 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+
+            string[] fileNames = new string[files.Count()];
+
+
+
+            var i = 0;
+
+            foreach (var item in files)
+            {
+                //FileInfo info = new FileInfo(files[item]);
+                FileInfo info = new FileInfo(files[i]);
+                fileNames[i] = Path.GetFileNameWithoutExtension(info.Name);
+                i++;
+            }
+
+
+            return View(fileNames);
+        }
+
+
+
+
+
+
+        public ActionResult DisplayContent(string id)
+        {
+            string selectedFile = Server.MapPath("~/TextFiles") + "\\" + id + ".txt";
+            TextFile modelFile = new TextFile();
+            modelFile.FileName = id + ".txt";
+            modelFile.FileContent = System.IO.File.ReadAllText(@selectedFile);
+
+
+            return View(modelFile);
+        }
+    }
+}
---
+++ Models/TextFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class TextFile
+    {
+        public string FileName { get; set; }
+        public string FileContent { get; set; }
+    }
+}
